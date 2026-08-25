6679cb82e99bb66452c13caf65d04a3c3753e114
obsessed3observer <mishramann98@gmail.com>
2026-08-25 22:41:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.71%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sptr = 0, tptr = 0 ;
+        while(sptr < s.size() && tptr < t.size()){
+            if(s[sptr] == t[tptr]){
+                sptr++ ;
+                tptr++ ;
+            }else{
+                tptr++ ;
+            }
+        }
+        return sptr == s.size() ;
+    }
+};
