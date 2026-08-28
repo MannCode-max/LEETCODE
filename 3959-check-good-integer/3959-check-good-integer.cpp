b5e154c42e4be05e283f4d16a77d804b689b4068
obsessed3observer <mishramann98@gmail.com>
2026-08-29 01:36:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (75.11%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkGoodInteger(int n) {
+        int digitSum = 0 , squareSum = 0 ;
+        int dig = 0 ; 
+        while(n>0){
+            dig = n % 10 ;
+            digitSum += dig ;
+            squareSum += dig * dig ;
+            n = n/10 ;
+        }
+        return squareSum - digitSum >= 50 ;
+    }
+};
