f3e66e9dc2b10c60026087420f1b208187d273dd
obsessed3observer <mishramann98@gmail.com>
2026-08-30 00:38:21 +0530

    Time: 2 ms (6.35%), Space: 9.2 MB (9.22%) - LeetHub

--- 1017-convert-to-base-2/1017-convert-to-base-2.cpp
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     string baseNeg2(int N) {
+        if (N == 0) return "0";
         string res = "";
-        while (N) {
-            res = to_string(N & 1) + res;
-            N = -(N >> 1);
+        
+        while (N != 0) {
+            int rem = N % -2;
+            N /= -2;
+            
+            // If the remainder is negative, we must fix it...
+            if (rem < 0) {
+                rem += 2;  // Change -1 remainder to +1
+                N += 1;    // Add 1 to the next number to balance it out
+            }
+            
+            res = to_string(rem) + res; //concatenation property lga di...
         }
-        return res == "" ? "0" : res;
+
+        return res;
     }
 };
