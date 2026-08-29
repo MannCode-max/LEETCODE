e9e87f3ff0668bf46e32c4a4ec59aaee07b3ca98
obsessed3observer <mishramann98@gmail.com>
2026-08-30 01:14:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.6%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
@@ -1,32 +1,21 @@
 class Solution {
+private:
+    string toBinary(int num) {
+        string binaryStr = "";
+        while (num != 0) {
+            binaryStr.push_back((num % 2) + '0');
+            num /= 2;
+        }
+        reverse(binaryStr.begin(), binaryStr.end());
+        return binaryStr;
+    }
+
 public:
     string convertDateToBinary(string date) {
-        int year = stoi(date.substr(0 , 4)) ;
-        int month = stoi(date.substr(5 , 2)) ;
-        int din = stoi(date.substr(8 , 2)) ;
-        string ans1 = "" ;
-        while(year != 0){
-            ans1.push_back((year % 2) + '0') ;
-            year = year/2 ;
-        }
-        reverse(ans1.begin() , ans1.end()) ;
-        ans1.push_back('-') ;
-
-        string ans2 = "" ;
-        while(month != 0){
-            ans2.push_back((month % 2) + '0') ;
-            month = month/2 ;
-        }
-        reverse(ans2.begin() , ans2.end()) ;
-        ans2.push_back('-') ;
-
-        string ans3 = "" ;
-        while(din != 0){
-            ans3.push_back((din % 2) + '0') ;
-            din = din/2 ;
-        }
-        reverse(ans3.begin() , ans3.end()) ;
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int din = stoi(date.substr(8, 2));
         
-        return (ans1+ans2+ans3) ;
+        return toBinary(year) + "-" + toBinary(month) + "-" + toBinary(din);
     }
 };
