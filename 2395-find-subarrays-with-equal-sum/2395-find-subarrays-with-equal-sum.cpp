8de3b72acdff199e7168f867bf194fa84f8dbd15
obsessed3observer <mishramann98@gmail.com>
2026-09-04 11:26:19 +0530

    Time: 0 ms (100%), Space: 10.9 MB (52.43%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool findSubarrays(vector<int>& nums) {
-        vector<int> sum ;
-        for(int i = 0 ; i < nums.size() - 1 ; i++){
-            int j = i+1 ;
-            sum.push_back(nums[i] + nums[j]) ;
-        }
-        sort(sum.begin() , sum.end()) ;
-        for(int i = 0 ; i < nums.size() - 2 ; i++){
-            if(sum[i] == sum[i+1]) return true ;
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            int sum = nums[i] + nums[i + 1];
+            if (s.count(sum)) return true;
+            s.insert(sum);
         }
         return false;
     }
