581504c46ac08fbe11aacaf1ee0f6056ec74f081
obsessed3observer <mishramann98@gmail.com>
2026-09-04 08:36:57 +0530

    Time: 3 ms (46.38%), Space: 7.8 MB (65.35%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int mat = 0 ;
+        while(n >= 2){
+            if(n % 2 == 0){
+                mat += n/2 ;
+                n = n / 2 ;
+            }else{
+                mat += n/2 ;
+                n = (n/2 + 1) ;
+            }
+        }
+        return mat ;
+    }
+};
