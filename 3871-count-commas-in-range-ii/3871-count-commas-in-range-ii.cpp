d29244df3248c45aa6206f0ea14a90b068dacc43
obsessed3observer <mishramann98@gmail.com>
2026-09-09 14:12:20 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,39 +1,10 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n < 1000) return 0;
-
-        long long comma = 0;
-
-        if(n < 1000000) {
-            comma += n - 1000 + 1;
+        long long ans=0;
+        for(long long i=1000;i<=n;i*=1000){
+            ans+=n-i+1;
         }
-        else {
-            comma += 999999 - 1000 + 1;
-
-            if(n < 1000000000) {
-                comma += 2LL * (n - 1000000 + 1);
-            }
-            else {
-                comma += 2LL * (999999999 - 1000000 + 1);
-
-                if(n < 1000000000000LL) {
-                    comma += 3LL * (n - 1000000000 + 1);
-                }
-                else {
-                    comma += 3LL * (999999999999LL - 1000000000 + 1);
-
-                    comma += 4LL * (
-                        min(n, 999999999999999LL)
-                        - 1000000000000LL + 1
-                    );
-
-                    if(n >= 1000000000000000LL)
-                        comma += 5;
-                }
-            }
-        }
-
-        return comma;
+        return ans;
     }
 };
